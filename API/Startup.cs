41078ce86e1ae3f21935aa64b09542260e53a546
Pete Kayuha <zzz8910@gmail.com>
2021-01-31 13:54:04 -0500

    Section 3 - Adding Cors to API

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,11 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -55,6 +60,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+            
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
