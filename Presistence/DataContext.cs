5ef517d7c47333ece48377be6a4fb21c9a0827aa
Pete Kayuha <zzz8910@gmail.com>
2021-02-14 13:33:32 -0500

    Updates to section one to reflect tutorials dotnet 5 updates

--- Presistence/DataContext.cs
+++ Presistence/DataContext.cs
@@ -11,6 +11,7 @@ namespace Presistence
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
