fdc3fbf0c9d7aa3c0b8d894a1ec257bdc2502deb
Pete Kayuha <zzz8910@gmail.com>
2024-06-01 14:55:26 -0400

    End of section 13 - client login and registration

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,12 +47,14 @@ namespace API.Controllers
         {
             if (await _userManager.Users.AnyAsync(x => x.UserName == register.Username))
             {
-                return BadRequest("Username already exists");
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem();
             }
 
             if (await _userManager.Users.AnyAsync(x => x.Email == register.Email))
             {
-                return BadRequest("Email already exists");
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem();
             }
 
             var user = new AppUser
